50d415bc2b4241e9c89856742eda76826264676c
Kanav Rohatgi <kanavr@gmail.com>
2026-04-13 14:08:19 +0530

    processing and saving recommendation from AI model

--- aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
@@ -1,10 +1,11 @@
 package com.fitness.aiservice.service;
 
 import com.fitness.aiservice.model.Activity;
+import com.fitness.aiservice.model.Recommendation;
+import com.fitness.aiservice.repository.RecommendationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,9 +13,15 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ActivityMessageListener {
 
+    private final ActivityAIService activityAIService;
+    private final RecommendationRepository recommendationRepository;
+
     @RabbitListener(queues = "activity.queue")
     public void processActivity(Activity activity){
         log.info("received activity for processing with id: {}", activity.getId());
+        //log.info("Generated Recommendation: {}", activityAIService.generateRecommendation(activity));
+        Recommendation recommendation = activityAIService.generateRecommendation(activity);
+        recommendationRepository.save(recommendation);
     }
 
 }
